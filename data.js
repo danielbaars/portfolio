7b6250833dd9008237211b6347093727a058378f
Daniel Baars <info@danielbaars.nl>
2017-05-29 11:30:07 +0200

    Big existing files upload

---
+++ data.js
@@ -0,0 +1,405 @@
+var clients = 
+{
+  "abn" : {
+    "shortName" : "abn",
+    "fullName" : "ABN AMRO",
+    "url" : "https://www.abnamro.nl/nl",
+    "logoPct" : 85
+  },
+  "achmea" : {
+    "shortName" : "achmea",
+    "fullName" : "Achmea",
+    "url" : "https://www.achmea.nl",
+    "logoPct" : 97
+  },
+  "adbirds" : {
+    "shortName" : "adbirds",
+    "fullName" : "AdBirds",
+    "url" : "http://www.adbirds.global/",
+    "logoPct" : 47
+  },
+  "agis" : {
+    "shortName" : "agis",
+    "fullName" : "Agis",
+    "url" : "https://www.achmea.nl",
+    "logoPct" : 70
+  },
+  "cb" : {
+    "shortName" : "cb",
+    "fullName" : "Centraal Beheer",
+    "url" : "https://www.centraalbeheer.nl",
+    "logoPct" : 53
+  },
+  "greens" : {
+    "shortName" : "greens",
+    "fullName" : "The Greens | EFA",
+    "url" : "https://www.greens-efa.eu/en/",
+    "logoPct" : 48
+  },
+  "knab" : {
+    "shortName" : "knab",
+    "fullName" : "Knab",
+    "url" : "https://www.knab.nl",
+    "logoPct" : 61
+  },
+  "kosmos" : {
+    "shortName" : "kosmos",
+    "fullName" : "Kosmos Uitgevers",
+    "url" : "http://www.kosmosuitgevers.nl/",
+    "logoPct" : 46
+  },
+  "kpn" : {
+    "shortName" : "kpn",
+    "fullName" : "KPN",
+    "url" : "https://www.kpn.com",
+    "logoPct" : 70
+  },
+  "master-it" : {
+    "shortName" : "master-it",
+    "fullName" : "Master it",
+    "url" : "http://www.master-it.nl/",
+    "logoPct" : 69
+  },
+  "modint" : {
+    "shortName" : "modint",
+    "fullName" : "Modint",
+    "url" : "http://modint.nl/",
+    "logoPct" : 75
+  },
+  "nlziet" : {
+    "shortName" : "nlziet",
+    "fullName" : "NLziet",
+    "url" : "https://www.nlziet.nl/nl",
+    "logoPct" : 69
+  },
+  "npo" : {
+    "shortName" : "npo",
+    "fullName" : "NPO",
+    "url" : "https://www.npo.nl/",
+    "logoPct" : 44
+  },
+  "play" : {
+    "shortName" : "play",
+    "fullName" : "Play van KPN",
+    "url" : "https://www.playvankpn.nl/",
+    "logoPct" : 58
+  },
+  "qicq" : {
+    "shortName" : "qicq",
+    "fullName" : "QicQ",
+    "url" : "https://www.qicq.nl/",
+    "logoPct" : 71
+  },
+  "rb" : {
+    "shortName" : "rb",
+    "fullName" : "RolandBerger",
+    "url" : "https://www.rolandberger.com/nl",
+    "logoPct" : 74
+  },
+  "shakies" : {
+    "shortName" : "shakies",
+    "fullName" : "Shakie's",
+    "url" : "http://retail.nsstations.nl/",
+    "logoPct" : 56
+  },
+  "simyo" : {
+    "shortName" : "simyo",
+    "fullName" : "Simyo",
+    "url" : "https://www.simyo.nl/",
+    "logoPct" : 74
+  },
+  "solarbird" : {
+    "shortName" : "solarbird",
+    "fullName" : "SolarBird",
+    "url" : "http://www.solarbird.nl/",
+    "logoPct" : 80
+  },
+  "thuiscomfort" : {
+    "shortName" : "thuiscomfort",
+    "fullName" : "Thuiscomfort",
+    "url" : "https://www.thuiscomfort.nl/",
+    "logoPct" : 88
+  },
+  "vv" : {
+    "shortName" : "vv",
+    "fullName" : "VakantieVeilingen",
+    "url" : "https://www.vakantieveilingen.nl/",
+    "logoPct" : 72
+  },
+  "wereldouders" : {
+    "shortName" : "wereldouders",
+    "fullName" : "WereldOuders",
+    "url" : "https://www.wereldouders.nl/",
+    "logoPct" : 100
+  },
+  "wijnbeurs" : {
+    "shortName" : "wijnbeurs",
+    "fullName" : "de Wijnbeurs",
+    "url" : "https://www.wijnbeurs.nl/",
+    "logoPct" : 85
+  },
+  "xs4all" : {
+    "shortName" : "xs4all",
+    "fullName" : "XS4ALL",
+    "url" : "https://www.xs4all.nl/",
+    "logoPct" : 62
+  },
+  "zk" : {
+    "shortName" : "zk",
+    "fullName" : "Zilveren Kruis",
+    "url" : "https://www.zilverenkruis.nl",
+    "logoPct" : 74
+  },
+  "rina" : {
+    "shortName" : "rina",
+    "fullName" : "Rina Mushonga",
+    "url" : "",
+    "logoPct" : ""
+  },
+  "doen-en-later" : {
+    "shortName" : "doen-en-later",
+    "fullName" : "Doen & Later",
+    "url" : "",
+    "logoPct" : ""
+  }
+};
+
+var projects = 
+{
+  "agis-productpage" : {
+    "shortName" : "agis-productpage",
+    "fullName" : "Agis productpage redesign",
+    "client" : "agis",
+    "mainSrc" : "130730-Agis-Salesfunnel-2013-1000-Templates-Basisverzekering.png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "agis-salesfunnel" : {
+    "shortName" : "agis-salesfunnel",
+    "fullName" : "Agis salesfunnel",
+    "client" : "agis",
+    "mainSrc" : "131205-Agis-Salesfunnel-Verleidingsprincipes-100-Wederkerigheid-Sowieso-Korting-100-small-v2a.png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "carolina-enw" : {
+    "shortName" : "carolina-enw",
+    "fullName" : "Carolina Bont book",
+    "client" : "kosmos",
+    "mainSrc" : "090729-Carolina-Bont-Intuitiepakket-Voorkant-2251x2000px-q10.jpg",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "cb-salesfunnel" : {
+    "shortName" : "cb-salesfunnel",
+    "fullName" : "Centraal Beheer salesfunnel",
+    "client" : "cb",
+    "mainSrc" : "131029-Centraal-Beheer-Cross-Sell-Funnels-Auto-Stap-1.png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "doen-en-later-banners" : {
+    "shortName" : "doen-en-later-banners",
+    "fullName" : "Doen & Later banners",
+    "client" : "doen-en-later",
+    "mainSrc" : "",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "knab-bannertest" : {
+    "shortName" : "knab-bannertest",
+    "fullName" : "Knab bannertest",
+    "client" : "knab",
+    "mainSrc" : "150825-Knab-Bannertest-Medium-Rectangle-300x250-OVERZICHT-v2.png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "kpn-hotspots-site" : {
+    "shortName" : "kpn-hotspots-site",
+    "fullName" : "KPN HotSpots website",
+    "client" : "kpn",
+    "mainSrc" : "150930-KPN-HotSpots-Van-De-Zaak-Homepage.png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "master-it-site" : {
+    "shortName" : "master-it-site",
+    "fullName" : "Master it website",
+    "client" : "master-it",
+    "mainSrc" : "140602-Master-It-Home-Page-DESKTOP.png",
+    "sourceCode" : "https://github.com/danielbaars/master-it-site",
+    "url" : "https://danielbaars.github.io/master-it-site/"
+  },
+  "modint-magazine" : {
+    "shortName" : "modint-magazine",
+    "fullName" : "Modint magazine",
+    "client" : "modint",
+    "mainSrc" : "091209-MODINT-Time-14-Cover-q10.jpg",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "npo-app" : {
+    "shortName" : "npo-app",
+    "fullName" : "NPO Plus app",
+    "client" : "npo",
+    "mainSrc" : "140417-NPO-Plus-Android-7in-Tablet-600dp-AFLEVERING.png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "play-banners-sound-speakers" : {
+    "shortName" : "play-banners-sound-speakers",
+    "fullName" : "Play van KPN banners",
+    "client" : "play",
+    "mainSrc" : "",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "play-lp-sound-speakers" : {
+    "shortName" : "play-lp-sound-speakers",
+    "fullName" : "Play van KPN landingpage",
+    "client" : "play",
+    "mainSrc" : "151125-Play-Sound-Speakers-LP-v3.png",
+    "sourceCode" : "https://github.com/danielbaars/play-van-kpn-lp-sound-speakers",
+    "url" : "https://danielbaars.github.io/play-van-kpn-lp-sound-speakers/"
+  },
+  "rb-book" : {
+    "shortName" : "rb-book",
+    "fullName" : "Roland Berger book",
+    "client" : "rb",
+    "mainSrc" : "111219a-Roland-Berger-Trojan-Horses-Of-Decline-Voorkant-1454x2048px.jpg",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "rina-cd" : {
+    "shortName" : "rina-cd",
+    "fullName" : "Rina Mushonga CD",
+    "client" : "rina",
+    "mainSrc" : "Rina-Mushonga-CD-Cover-Flyer.png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "shakies-print" : {
+    "shortName" : "shakies-print",
+    "fullName" : "Shakie's flyers",
+    "client" : "shakies",
+    "mainSrc" : "Shakies-Vouchers.png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "simyo-banners-best" : {
+    "shortName" : "simyo-banners-best",
+    "fullName" : "Simyo banners",
+    "client" : "simyo",
+    "mainSrc" : "",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "simyo-banners-consumentenbond" : {
+    "shortName" : "simyo-banners-consumentenbond",
+    "fullName" : "Simyo banners",
+    "client" : "simyo",
+    "mainSrc" : "",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "simyo-homepage" : {
+    "shortName" : "simyo-homepage",
+    "fullName" : "Simyo homepage redesign",
+    "client" : "simyo",
+    "mainSrc" : "150901-Simyo-Homepage-Redesign-v4s-1024x1548px.png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "simyo-lp" : {
+    "shortName" : "simyo-lp",
+    "fullName" : "Simyo landingpage",
+    "client" : "simyo",
+    "mainSrc" : "150610-Simyo-Spontane-Beslisser-LP-RETINA.png",
+    "sourceCode" : "https://github.com/danielbaars/simyo-lp-spontane-beslisser",
+    "url" : "https://danielbaars.github.io/simyo-lp-spontane-beslisser/"
+  },
+  "solarbird-site" : {
+    "shortName" : "solarbird-site",
+    "fullName" : "Solarbird website",
+    "client" : "solarbird",
+    "mainSrc" : "130428-solarbird.nl-Home-Slide-2-Screenshot.png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "vv-lp-supercadeaus" : {
+    "shortName" : "vv-lp-supercadeaus",
+    "fullName" : "VakantieVeilingen landingpage",
+    "client" : "vv",
+    "mainSrc" : "151207-VakantieVeilingen-Landingpagina-v2.png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "xs4all-lp" : {
+    "shortName" : "xs4all-lp",
+    "fullName" : "XS4ALL landingpage",
+    "client" : "xs4all",
+    "mainSrc" : "131103-XS4ALL-Triple-Play-(Gweezy-v5).png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "zk-lp-laatste-bod" : {
+    "shortName" : "zk-lp-laatste-bod",
+    "fullName" : "Zilveren Kruis landingpage",
+    "client" : "zk",
+    "mainSrc" : "150113-XSell-Laatste-Bod-LP.png",
+    "sourceCode" : "",
+    "url" : ""
+  },
+  "zk-lp-micro" : {
+    "shortName" : "zk-lp-micro",
+    "fullName" : "Zilveren Kruis landingpage",
+    "client" : "zk",
+    "mainSrc" : "150426-Achmea-Xsell-Thematische-Microcampagne-LP-DESKTOP-Gezinnen-Oudere-Kinderen.png",
+    "sourceCode" : "https://github.com/danielbaars/zk-cb-xsell-lp",
+    "url" : "https://danielbaars.github.io/zk-cb-xsell-lp/woon-micro-q2-pubers.html"
+  },
+  "zk-lp-woon" : {
+    "shortName" : "zk-lp-woon",
+    "fullName" : "Zilveren Kruis landingpage",
+    "client" : "zk",
+    "mainSrc" : "150420-Achmea-Xsell-Woonverzekering-LP-Q2-Nieuw.png",
+    "sourceCode" : "",
+    "url" : ""
+  }
+};
+
+var fcc =
+{
+  "fcc-simon-game" : {
+    "shortName" : "fcc-simon-game",
+    "fullName" : "Simon Game"
+  },
+  "fcc-tic-tac-toe" : {
+    "shortName" : "fcc-tic-tac-toe",
+    "fullName" : "Tic Tac Toe Game"
+  },
+  "fcc-calculator" : {
+    "shortName" : "fcc-calculator",
+    "fullName" : "JavaScript Calculator"
+  },
+  "fcc-pomodoro" : {
+    "shortName" : "fcc-pomodoro",
+    "fullName" : "Pomodoro Clock"
+  },
+  "fcc-quote-machine" : {
+    "shortName" : "fcc-quote-machine",
+    "fullName" : "Random Quote Machine"
+  },
+  "fcc-local-weather" : {
+    "shortName" : "fcc-local-weather",
+    "fullName" : "Show the Local Weather"
+  },
+  "fcc-wikipedia-viewer" : {
+    "shortName" : "fcc-wikipedia-viewer",
+    "fullName" : "Wikipedia Viewer"
+  },
+  "fcc-twitch-tv" : {
+    "shortName" : "fcc-twitch-tv",
+    "fullName" : "Use the Twitchtv JSON API"
+  }
+};
